09501430dde94c2618b2c300b451ae399beb1469
Axiarcu1 <giang0351@gmail.com>
2023-11-27 21:36:39 +0700

    test 1.0
    run tutorial code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,12 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+<<<<<<< Updated upstream
 
+=======
+#include "stdio.h"
+#include "stdlib.h"
+>>>>>>> Stashed changes
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -42,7 +47,7 @@
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 
-TIM_HandleTypeDef htim3;
+TIM_HandleTypeDef htim2;
 
 UART_HandleTypeDef huart2;
 
@@ -54,16 +59,25 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC1_Init(void);
-static void MX_TIM3_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+<<<<<<< Updated upstream
 uint8_t temp = 0;
 void HAL_UART_RxCpltCallback ( UART_HandleTypeDef * huart ) {
+=======
+char str[50];
+uint8_t temp = 0;
+//uint8_t buffer[MAX_BUFFER_SIZE];
+//uint8_t index_buffer = 0;
+//uint8_t buffer_flag = 0;
+void HAL_UART_RxCpltCallback (UART_HandleTypeDef * huart) {
+>>>>>>> Stashed changes
 	if(huart->Instance == USART2 ) {
 		HAL_UART_Transmit(&huart2 ,&temp ,1 ,50);
 		HAL_UART_Receive_IT(&huart2, &temp ,1);
@@ -100,23 +114,36 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC1_Init();
-  MX_TIM3_Init();
   MX_USART2_UART_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   HAL_UART_Receive_IT(&huart2, &temp, 1);
   /* USER CODE END 2 */
-  uint32_t ADC_value = 0;
+
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  uint32_t ADC_value = 0;
+  HAL_ADC_Start(&hadc1);
   while (1)
   {
     /* USER CODE END WHILE */
+<<<<<<< Updated upstream
 	  HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
 	  ADC_value = HAL_ADC_GetValue(&hadc1) ;
 	  HAL_UART_Transmit (&huart2, (void *) str, sprintf(str, "%d\n", ADC_value), 1000);
 	  HAL_Delay(500);
+=======
+	  HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin ) ;
+
+	  ADC_value = HAL_ADC_GetValue(&hadc1);
+	  //ADC_value = 2.5;
+	  HAL_UART_Transmit (&huart2 , (void *)str , sprintf(str , "%d\n", ADC_value ) , 1000);
+
+	  HAL_Delay (500) ;
+>>>>>>> Stashed changes
     /* USER CODE BEGIN 3 */
   }
+  HAL_ADC_Stop(&hadc1);
   /* USER CODE END 3 */
 }
 
@@ -211,47 +238,47 @@ static void MX_ADC1_Init(void)
 }
 
 /**
-  * @brief TIM3 Initialization Function
+  * @brief TIM2 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_TIM3_Init(void)
+static void MX_TIM2_Init(void)
 {
 
-  /* USER CODE BEGIN TIM3_Init 0 */
+  /* USER CODE BEGIN TIM2_Init 0 */
 
-  /* USER CODE END TIM3_Init 0 */
+  /* USER CODE END TIM2_Init 0 */
 
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
 
-  /* USER CODE BEGIN TIM3_Init 1 */
+  /* USER CODE BEGIN TIM2_Init 1 */
 
-  /* USER CODE END TIM3_Init 1 */
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 7999;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 9;
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 7999;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 9;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
   {
     Error_Handler();
   }
   sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
   {
     Error_Handler();
   }
   sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
   sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM3_Init 2 */
+  /* USER CODE BEGIN TIM2_Init 2 */
 
-  /* USER CODE END TIM3_Init 2 */
+  /* USER CODE END TIM2_Init 2 */
 
 }
 
